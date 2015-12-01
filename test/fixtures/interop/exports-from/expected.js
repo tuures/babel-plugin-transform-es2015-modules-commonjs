962fa8f3e1bac72984ee0fe641c259a49a08758a
Tuure Savuoja <tuure.savuoja@trademarknow.com>
2015-12-01 15:40:39 +0200

    fork

---
+++ test/fixtures/interop/exports-from/expected.js
@@ -0,0 +1,62 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _foo = require("foo");
+
+for (var _key in _foo) {
+  if (_key === "default") continue;
+  Object.defineProperty(exports, _key, {
+    enumerable: true,
+    get: (function (k) {
+      return function () {
+        return _foo[k];
+      };
+    })(_key)
+  });
+}
+
+Object.defineProperty(exports, "foo", {
+  enumerable: true,
+  get: function () {
+    return _foo.foo;
+  }
+});
+Object.defineProperty(exports, "foo", {
+  enumerable: true,
+  get: function () {
+    return _foo.foo;
+  }
+});
+Object.defineProperty(exports, "bar", {
+  enumerable: true,
+  get: function () {
+    return _foo.bar;
+  }
+});
+Object.defineProperty(exports, "bar", {
+  enumerable: true,
+  get: function () {
+    return _foo.foo;
+  }
+});
+Object.defineProperty(exports, "default", {
+  enumerable: true,
+  get: function () {
+    return _foo.foo;
+  }
+});
+Object.defineProperty(exports, "default", {
+  enumerable: true,
+  get: function () {
+    return _foo.foo;
+  }
+});
+Object.defineProperty(exports, "bar", {
+  enumerable: true,
+  get: function () {
+    return _foo.bar;
+  }
+});
---
+++ test/fixtures/strict/import/expected.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _foo = require("foo");
+
+_foo.default;
+_foo.default;
+_foo.foo3;
